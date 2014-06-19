d25aa15815e5dc59cfab053668b6a9cb15da0273
Pravin Mishra <pravin.kumar@shephertz.co.in>
2014-06-19 15:58:05 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -17,10 +17,10 @@ module.exports = {
     },
     production: {
         driver: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '',
-        database: 'compoundjs_production'
+        host: '54.208.130.57',
+        port: 53653,
+        username: 'aqfjgj73vsnmddoi',
+        password: 'acavceal34mvrqy3bry17bsain3gkw16',
+        database: 'demo_db'
     }
 };
